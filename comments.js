81595c6c65e720d30a94c62c520cd2d7e4610261
tommaho <tom@tommaho.com>
2023-11-16 19:44:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,47 +1,33 @@
-//create web server
-const express = require('express');
-const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-const mongoose = require('mongoose');
-const {Comment} = require('./models');
-const {DATABASE_URL, PORT} = require('./config');
+// Create web server
+// 1. Create a web server
+// 2. Read the comments.json file
+// 3. Convert the file to a JavaScript object
+// 4. Send the object to the client
 
-app.use(express.static('public'));
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-//GET request
-app.get('/comments', (req, res) => {
-  Comment
-    .find()
-    .limit(10)
-    .then(comments => {
-      res.json({
-        comments: comments.map(
-          (comment) => comment.serialize())
-      });
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({message: 'Internal server error'})
-    });
+const port = 3000;
+const commentsPath = path.join(__dirname, "comments.json");
+
+// 1. Create a web server
+const server = http.createServer((req, res) => {
+  // 2. Read the comments.json file
+  fs.readFile(commentsPath, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal server error");
+    } else {
+      // 3. Convert the file to a JavaScript object
+      const comments = JSON.parse(data);
+
+      // 4. Send the object to the client
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(comments));
+    }
+  });
 });
 
-//POST request
-app.post('/comments', jsonParser, (req, res) => {
-  const requiredFields = ['title', 'content', 'author'];
-  //check if all required fields are present
-  for (let i = 0; i < requiredFields.length; i++) {
-    const field = requiredFields[i];
-    //if any field is missing, send error message
-    if (!(field in req.body)) {
-      const message = `Missing \'${field}\' in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-
-  Comment
-    .create({
-      title: req.body.title,
-      content: req.body.content,
+server.listen(port, () => console.log(`Server listening on port ${port}`));
