ad94e76383162b0b3ccb9e2a5552d7212f080eb9
tommaho <tom@tommaho.com>
2023-11-16 19:11:18 +0000

    third copilot commit

---
+++ comments.js
@@ -0,0 +1,32 @@
+//create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//set view engine
+app.set('view engine', 'ejs');
+
+//set public folder
+app.use(express.static('public'));
+
+//use body parser
+app.use(express.urlencoded({ extended: true }));
+
+//use method override
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+//connect to mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//define schema
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now },
+});
+const Blog = mongoose.model('Blog', blogSchema);
+
+const commentSchema = new mongoose.Schema({
